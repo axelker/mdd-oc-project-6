17982311e8dbf4837e42c211c045e616b8dc6055
axelker <axelker@outlook.fr>
2025-02-26 14:17:03 +0100

    feat: add dto

--- back/src/main/java/com/openclassrooms/mdd/dto/request/AuthLoginRequest.java
+++ back/src/main/java/com/openclassrooms/mdd/dto/request/AuthLoginRequest.java
@@ -2,15 +2,23 @@ package com.openclassrooms.mdd.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class AuthLoginRequest {
+
     @NotNull(message = "email is required.")
     @Email
     private String email;
-    @NotNull(message = "password is required.")
+
+    @Size(min = 8, message = "Password must be at least 8 characters long.")
+    @Pattern(
+        regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$",
+        message = "Password must contain at least one digit, one lowercase letter, one uppercase letter, and one special character."
+    )
     private String password;
 }
--- back/src/main/java/com/openclassrooms/mdd/dto/response/ThemeResponse.java
+++ back/src/main/java/com/openclassrooms/mdd/dto/response/ThemeResponse.java
@@ -1,12 +1,14 @@
 package com.openclassrooms.mdd.dto.response;
 
-import java.util.List;
-
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-public class RentalsResponse {
-    List<RentalResponse> rentals;
+public class ThemeResponse {
+    private Long id;
+
+    private String name;
+
+    private String description;
 }
