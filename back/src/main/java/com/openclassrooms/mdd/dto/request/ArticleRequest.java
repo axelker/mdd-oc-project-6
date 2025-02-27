db7027db8a38c412a815675660fde8622a47a073
axelker <axelker@outlook.fr>
2025-02-27 15:01:11 +0100

    fix: use size for string validation instead max

--- back/src/main/java/com/openclassrooms/mdd/dto/request/ArticleRequest.java
+++ back/src/main/java/com/openclassrooms/mdd/dto/request/ArticleRequest.java
@@ -1,8 +1,8 @@
 package com.openclassrooms.mdd.dto.request;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,11 +11,11 @@ import lombok.Getter;
 public class ArticleRequest {
 
     @NotBlank
-    @Max(value=255, message = "Article name must be at max 255 characters long.")
+    @Size(max = 255, message = "Article name must be at max 255 characters long.")
     private String name;
 
     @NotBlank
-    @Max(value=2000, message = "Article name must be at max 2000 characters long.")
+    @Size(max = 255, message = "Article name must be at max 2000 characters long.")
     private String description;
 
     @NotNull
