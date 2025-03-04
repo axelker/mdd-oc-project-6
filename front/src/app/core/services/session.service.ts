5f552e4fcc09e9226910b1469909d0edd0807c5b
axelker <axelker@outlook.fr>
2025-03-04 10:34:45 +0100

    test: add dumb impl for session service

--- front/src/app/core/services/session.service.ts
+++ front/src/app/core/services/session.service.ts
@@ -1,24 +1,25 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
-  public isLogged = signal(false);
   private TOKEN_SESSION_KEY: string = "token";
   constructor() { }
 
   public getToken() : string {
     return localStorage.getItem(this.TOKEN_SESSION_KEY) ?? '';
   }
-
+  
+  public isLogged():boolean {
+    return !!this.getToken();
+  }
+  
   public logUser(token:string):void {
-    this.isLogged.set(true);
     localStorage.setItem(this.TOKEN_SESSION_KEY,token);
   }
 
   public logOutUser():void {
-    this.isLogged.set(false);
     localStorage.removeItem(this.TOKEN_SESSION_KEY);
   }
 }
