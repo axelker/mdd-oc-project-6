81f7368c1a63c5d27f489d98a9b20d52aec9d28b
axelker <axelker@outlook.fr>
2025-03-04 11:36:12 +0100

    feat: move user interface

--- front/src/app/features/user/services/user.service.ts
+++ front/src/app/features/user/services/user.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { User } from './interfaces/user';
+import { User } from '../interfaces/user';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private basePath:string = "/api/user";
+  private basePath:string = "/api/users";
   constructor(private http:HttpClient) { }
 
   public getUserById(id: number): Observable<User> {
