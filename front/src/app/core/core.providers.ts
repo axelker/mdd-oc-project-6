bff1b07361c8e943d3f202199a30a5c64f347647
axelker <axelker@outlook.fr>
2025-03-04 11:35:27 +0100

    feat: improve http error management and rename jwt interceptor

--- front/src/app/core/core.providers.ts
+++ front/src/app/core/core.providers.ts
@@ -1,9 +1,10 @@
 import { EnvironmentProviders, makeEnvironmentProviders } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { jwtInterceptor } from './interceptors/jwt-interceptor.service';
+import { jwtInterceptor } from './interceptors/jwt-interceptor';
+import { httpErrorInterceptor } from './interceptors/http-error-interceptor';
 
 export function provideCore(): EnvironmentProviders {
   return makeEnvironmentProviders([
-    provideHttpClient(withInterceptors([jwtInterceptor]))
+    provideHttpClient(withInterceptors([jwtInterceptor,httpErrorInterceptor]))
   ]);
 }
