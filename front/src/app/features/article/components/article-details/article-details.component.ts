b5d39da52a401b51dee2bcf7da93bdf7089e0d97
axelker <axelker@outlook.fr>
2025-03-04 23:07:18 +0100

    feat: finalize article details

--- front/src/app/features/article/components/article-details/article-details.component.ts
+++ front/src/app/features/article/components/article-details/article-details.component.ts
@@ -1,15 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../../interfaces/article';
+import { ArticleService } from '../../services/article.service';
+import { EMPTY, Observable } from 'rxjs';
+import { AsyncPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-article-details',
   standalone: true,
+  imports:[AsyncPipe,DatePipe],
   templateUrl: './article-details.component.html',
-  styleUrl: './article-details.component.scss'
+  styleUrl: './article-details.component.scss',
 })
-export class ArticleDetailsComponent {
- @Input({required:true}) article!: Article;
+export class ArticleDetailsComponent implements OnInit {
+  @Input({required:true}) articleId!:number;
+  article$: Observable<Article> = EMPTY;
 
-  constructor() {}
-  
+  constructor(
+    private articleService: ArticleService,
+  ) {}
+  ngOnInit(): void {
+    this.article$ = this.articleService.getArticleById(this.articleId);
+  }
 }
--- front/src/app/features/article/pages/article-details-page/article-details-page.component.ts
+++ front/src/app/features/article/pages/article-details-page/article-details-page.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticleDetailsComponent } from "../../components/article-details/article-details.component";
+import { PageHeaderComponent } from "../../../../shared/components/page-header/page-header.component";
+import { ActivatedRoute } from '@angular/router';
+import { CommentListComponent } from "../../components/comment-list/comment-list.component";
+import { CommentFormComponent } from "../../components/comment-form/comment-form.component";
 
 @Component({
   selector: 'app-article-details-page',
   standalone: true,
-  imports: [],
+  imports: [ArticleDetailsComponent, PageHeaderComponent, CommentListComponent, CommentFormComponent],
   templateUrl: './article-details-page.component.html',
   styleUrl: './article-details-page.component.scss'
 })
-export class ArticleDetailsPageComponent {
+export class ArticleDetailsPageComponent implements OnInit {
+  articleId!:number;
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.articleId = this.activatedRoute.snapshot.params['id'];
+  }
 
 }
