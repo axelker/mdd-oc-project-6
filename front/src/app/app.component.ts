81126c84b5cac194b9363720a099c11f98fcc4c5
axelker <axelker@outlook.fr>
2025-03-06 12:25:21 +0100

    feat: finalize nav bar component

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from "./shared/components/nav-bar/nav-bar.component";
+import { NavBarComponent } from "./shared/components/nav/nav-bar/nav-bar.component";
 import { SessionService } from './core/services/session.service';
 
 @Component({
