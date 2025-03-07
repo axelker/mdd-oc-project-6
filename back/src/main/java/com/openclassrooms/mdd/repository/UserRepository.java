bf1c82ad1b37e063f3431c5c624d11aa8e1dcd82
axelker <axelker@outlook.fr>
2025-03-07 18:25:49 +0100

    docs: add java doc for repository

--- back/src/main/java/com/openclassrooms/mdd/repository/UserRepository.java
+++ back/src/main/java/com/openclassrooms/mdd/repository/UserRepository.java
@@ -6,7 +6,29 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.openclassrooms.mdd.model.UserEntity;
 
+/**
+ * Repository interface for managing {@link UserEntity} persistence.
+ * <p>
+ * This interface extends {@link CrudRepository} to provide basic CRUD operations
+ * for the {@code users} table. Custom query methods are included for retrieving users
+ * by email or username.
+ * </p>
+ */
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    /**
+     * Finds a user by their email address.
+     *
+     * @param email the email of the user.
+     * @return an {@link Optional} containing the user if found, otherwise empty.
+     */
     Optional<UserEntity> findByEmail(String email);
+
+    /**
+     * Finds a user by their username.
+     *
+     * @param username the username of the user.
+     * @return an {@link Optional} containing the user if found, otherwise empty.
+     */
     Optional<UserEntity> findByUsername(String username);
 }
