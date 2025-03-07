44d30c5e2a4661cd10ed31d31bdecfe446be1006
axelker <axelker@outlook.fr>
2025-03-07 10:07:39 +0100

    feat: add validation jwt and generate jwt cookie

--- back/src/main/java/com/openclassrooms/mdd/service/auth/JWTService.java
+++ back/src/main/java/com/openclassrooms/mdd/service/auth/JWTService.java
@@ -2,14 +2,19 @@ package com.openclassrooms.mdd.service.auth;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwsHeader;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.stereotype.Service;
 
 import com.openclassrooms.mdd.model.UserEntity;
@@ -23,13 +28,60 @@ public class JWTService {
     @Value("${jwt.expiration}")
     private long jwtExpiration;
 
+    @Autowired
     private JwtEncoder jwtEncoder;
+
+    @Autowired
+    private JwtDecoder jwtDecoder;
 
     public JWTService(JwtEncoder jwtEncoder) {
         this.jwtEncoder = jwtEncoder;
     }
 
-    public String generateToken(UserEntity user) {
+    public Long getUserId(Authentication authentication) {
+        if (authentication.getPrincipal() instanceof Jwt jwt) {
+            return jwt.getClaim("userId");
+        } else if (authentication.getPrincipal() instanceof UserEntity user) {
+            return user.getId();
+        }
+        throw new IllegalStateException("Authentication object is invalid.");
+    }
+    
+
+    public String extractEmail(String token) {
+        return jwtDecoder.decode(token).getClaim("sub");
+    }
+    
+
+    public ResponseCookie generateJwtCookie(UserEntity user) {
+        String token = generateToken(user);
+        return ResponseCookie.from("jwt", token)
+                .httpOnly(true)
+                .path("/")
+                .maxAge(3600)
+                .sameSite("Strict")
+                .build();
+    }
+
+    public ResponseCookie generateJwtLogoutCookie() {
+        return ResponseCookie.from("jwt", "")
+                .httpOnly(true)
+                .path("/")
+                .maxAge(3600)
+                .sameSite("Strict")
+                .build();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            jwtDecoder.decode(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    private String generateToken(UserEntity user) {
         Instant now = Instant.now();
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuer(jwtIssuer)
@@ -42,11 +94,4 @@ public class JWTService {
                 .from(JwsHeader.with(MacAlgorithm.HS256).build(), claims);
         return this.jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
     }
-
-    public Long getUserId(Authentication authentication) {
-        Jwt jwt = (Jwt) authentication.getPrincipal();
-        Long userIdLong = jwt.getClaim("userId");
-        return Long.valueOf(userIdLong);
-    }
-
 }
