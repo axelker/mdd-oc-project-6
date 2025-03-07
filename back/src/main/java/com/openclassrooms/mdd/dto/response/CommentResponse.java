cf4e8a7e53133846a5398507c05cfb27191317c6
axelker <axelker@outlook.fr>
2025-03-07 17:55:27 +0100

    docs: add java doc for DTO package

--- back/src/main/java/com/openclassrooms/mdd/dto/response/CommentResponse.java
+++ back/src/main/java/com/openclassrooms/mdd/dto/response/CommentResponse.java
@@ -3,10 +3,31 @@ package com.openclassrooms.mdd.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * DTO representing a comment response.
+ * <p>
+ * This class is used to transfer comment details in API responses.
+ * </p>
+ */
 @Getter
 @Builder
 public class CommentResponse {
+
+    /**
+     * The unique identifier of the comment.
+     */
     private Long id;
+
+    /**
+     * The owner (author) of the comment.
+     * <p>
+     * Represented as a {@link SummaryResponse} containing minimal user details.
+     * </p>
+     */
     private SummaryResponse owner;
+
+    /**
+     * The content of the comment.
+     */
     private String message;
 }
--- back/src/main/java/com/openclassrooms/mdd/dto/response/Response.java
+++ back/src/main/java/com/openclassrooms/mdd/dto/response/Response.java
@@ -3,8 +3,18 @@ package com.openclassrooms.mdd.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * DTO representing a generic response message.
+ * <p>
+ * This class is used to return standard API responses containing a message.
+ * </p>
+ */
 @Getter
 @Builder
 public class Response {
+
+    /**
+     * The response message providing feedback on the operation.
+     */
     private String message;
 }
