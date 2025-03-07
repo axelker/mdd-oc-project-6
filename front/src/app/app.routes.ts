7a3ab1392b5c6e204bb99df3f2b4bfcd4e71e67c
axelker <axelker@outlook.fr>
2025-03-07 12:01:54 +0100

    feat: add auth layout comp for manage authenticated app diplaying

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,33 +2,42 @@ import { Routes } from '@angular/router';
 import { canActivateUnAuth } from './core/guards/unauth.guard';
 import { canActivateAuth } from './core/guards/auth.guard';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { AuthLayoutComponent } from './core/layout/auth-layout/auth-layout.component';
 
 export const routes: Routes = [
-    {
-        path:'', 
-        redirectTo: 'articles', 
-        pathMatch: 'full' 
-    },
-    {
-        path:'articles',
-        loadChildren: () => import('./features/article/article.routes').then((v) => v.routes),
-        canActivate:[canActivateAuth]
-    },
-    {
-        path:'themes',
-        loadChildren: () => import('./features/theme/theme.routes').then((v) => v.routes),
-        canActivate:[canActivateAuth]
-    },
-    {
-        path:'users',
-        loadChildren: () => import('./features/user/user.routes').then((v) => v.routes),
-        canActivate:[canActivateAuth]
-    },
-    {
-        path:'auth',
-        loadChildren: () => import('./features/auth/auth.routes').then((v) => v.routes),
-        canActivate:[canActivateUnAuth]
-    },
-    { path: '404', component: NotFoundComponent },
-    { path: '**', redirectTo: '404' }
+  {
+    path: '',
+    canActivate: [canActivateAuth],
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'articles',
+        pathMatch: 'full',
+      },
+      {
+        path: 'articles',
+        loadChildren: () =>
+          import('./features/article/article.routes').then((v) => v.routes),
+      },
+      {
+        path: 'themes',
+        loadChildren: () =>
+          import('./features/theme/theme.routes').then((v) => v.routes),
+      },
+      {
+        path: 'users',
+        loadChildren: () =>
+          import('./features/user/user.routes').then((v) => v.routes),
+      },
+    ],
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.routes').then((v) => v.routes),
+    canActivate: [canActivateUnAuth],
+  },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '404' },
 ];
